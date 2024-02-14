9b8c3e8b59d8ec67b62019dd18914b57633464e1
FilippoITS <guerrierofilippo3@gmail.com>
2024-02-14 18:55:45 +0100

    prime regole

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Exercises-on-Python-Fundamentals
-## secondo paragrafo
+1. per mettere i numeri con la virgola bisogna usare il punto
+2. posso assegnare variabili così: esempio = 10, esempio = "parola"
+3. (per parole vanno usati "i doppi apici")
+4. con triplo apice(""") creo un commento multiriga che viene esuguito ma tanto non fa nulla
+5. == fa una verifica se quello detto dopo è vero o falso
+6. read eval print loop REPL (python funziona così)
+7. Una funzione può essere associata ad una variabile (a = print)
+8. La funzione input da la possibilità di assegnare le variabili dopo sul terminale ed è sempre unas tringa a meno che non venga specificato prima
+9. 
---
+++ Secondo.py
@@ -0,0 +1,43 @@
+paperino = "30"
+ziopaperone = "True"
+gastone = False
+peppe = "namios" #ora peppe sarà diventanto namios ( ha preso quel valore)
+Numero = 2311312.2 #mi darà questo numero
+numoro2 = 1212. * .9 + .4 #mi darà i numeri moltiplicati e addizzionati
+c = 10
+a = c ==17  #a prenderà il valore di falso perchè c non ha calore 17
+
+
+i1 = 5
+i2 = 7
+s1 = "salve"
+s2 = "ciao"
+f1 = 10.3
+f2 = 12.89
+b1 = True
+b2 = False
+
+print(i1+i2) #sto addizziondando due valori interi che ho chiamato rispettivamente i1 e  12
+print(s1+s2) #sto addizzionando due stringe"""s1 e s2
+print(f1+f2) #sto addizzionando due numeri "" f1 e f2
+print(b1+b2) #sto addizzionandi due valori booleani """"b1 e b2 (impossibile perchè sono due booleani)
+#print(i1+s1)(non posso + una stringa e un numero)
+#print(s1+i2) (non posso + una stringa e un numero)
+print(i1+f2) #sto addizzionando un numero intero e uno con la virgola
+print(f2+i2) #sto addizzionando un numero con la virgola e uno intero
+print(i1+b1) #sto addizzionando un valore booleano e un valore intero(
+print(f1+b1) #sto addizzionando un numero con la virgola e un booleano
+#print(s1+b1) (no+ fra stringhe e booleani)
+print(b1+i1) #sto addizzionando un valore booleano e un numero intero
+#print(b1+s1) (no+ fra booleani e stringhe)
+print(b1+f1) #sto addizzionandoun valore booleano e un float
+"""
+se addizziono a un numero il booleano true aggiungo uno al risultato 
+se addizziono a un numero il booleano false aggiungo 0 al risultato
+"""
+
+a = print #read me 7
+a("ciao")
+a = input() #read me 8
+print ("a:",a)
+a = int(input("inserire un numero:")) #vedere read me 8
