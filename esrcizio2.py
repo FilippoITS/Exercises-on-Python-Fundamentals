3bdbe5099a290da918175fd065effcdfcd7a7028
FilippoITS <159887406+FilippoITS@users.noreply.github.com>
2024-02-20 11:47:54 +0100

    esrcizio2.py
    es finito

--- esrcizio2.py
+++ esrcizio2.py
@@ -1,15 +1,28 @@
-r1 = 30 #cm
-r2 = 45 #cm
-r3 = 33 #cm
+r1 = int(input("RaggioN1: ")) #30cm
+r2 = int(input("RaggioN2: ")) #45cm
+r3 = int(input("RaggioN3: ")) #33cm
 
-h = 130 #cm
+h = int(input("altezza: ")) #130cm
 
 pi = 3.1415
 
 s1 = pi * (r1 **2)
+
 s2 = pi * (r2 **2)
 s3 = pi * (r3 **2)
 s2m4 = s2 * 4
 V = (1 / 6) * h  * (s1 + s2m4 + s3)
-VolumeInLitri = ( V / 1000 )
-print(VolumeInLitri)  
+Vl = ( V / 1000 )
+print("VolumeInLitri: " +str (Vl))  #in litri
+
+
+
+
+r1s = r1**2
+r2s = r2**2
+r1pr2 = r1 * r2
+
+Abotte = ((r1s + r2s + r1pr2) * pi) + (r1 + r2) * h
+print("AreaBotte: " +str (Abotte)) 
+
+
